299ee5f7ca3ee3d26858cc905c1391cb2008482f
samarijo <sabrinamariano1994@hotmail.com>
2022-07-26 09:24:44 -0300

    Primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
+function validadorCard (){
+    let cardNumb = document.getElementById("numDigitado").value
+    let resultFinal = validator.isValid(cardNumb)
+    console.log(resultFinal)
+    let mascarar = validator.maskify(cardNumb)
+    let resultmask = document.getElementById("numMask")
+    
+    if (cardNumb === ""){
+        return resultmask.textContent = " ENTER A NUMBER"
+    }
+    if (resultFinal) {
+        resultmask.textContent = mascarar + " VALID CARD" 
+    }
+    else {
+        resultmask.textContent = mascarar + " INVALID CARD"
+    }
+    document.getElementById("numDigitado").value = ""
 
-console.log(validator);
+}
+let runBotao = document.getElementById("botao")
+runBotao.addEventListener("click", validadorCard)
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
 const validator = {
-  // ...
-};
+  isValid:
+  function (logicaString){
+    let cardNumb = logicaString.split("").reverse()
+    let sumFinal = 0;
 
+    for (let i=0; i < cardNumb.length;i++) { 
+      let num = parseInt(cardNumb[i])
+      if ((i % 2 !== 0) && (num>=5)){
+        sumFinal = sumFinal + ((num*2)-9);
+      }
+      else if ((i % 2 !== 0) && (num<=5)) {
+        sumFinal = sumFinal + (num *2);
+      }
+      else {
+        sumFinal = sumFinal + (num);
+      }
+    }
+    if (sumFinal % 10  === 0){
+      return true
+    }
+    else {
+      return false
+    }
+
+  },
+  maskify: function (mascararNum) {
+    const ultimosQuatroNum = 4
+    let mascarar = ""
+    for (let i = 0; i < mascararNum.length; i++) {
+      if (i >= ((mascararNum.length) - ultimosQuatroNum)) {
+        mascarar = mascarar + mascararNum.charAt(i)
+      }
+      else {
+        mascarar = mascarar + "#"
+      }
+    }
+    return mascarar
+  }
+}
 export default validator;
